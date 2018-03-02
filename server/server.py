6e05fd39e365b320398d430e502d7b4f1b462dbb
Julian Libor <julian.libor@gmail.com>
2018-03-02 15:26:23 +0100

    add static file server

--- server/server.py
+++ server/server.py
@@ -6,38 +6,29 @@ Created on Thu Feb 27 09:25:24 2018
 ### Aktuelle Version als Hilfe ausgeben
 import os
 import time 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer       # python 2
+# from http.server import BaseHTTPRequestHandler, HTTPServer        # python 3
 import json
+from compute_embedding import compute_graph, initialise_graph
 
-## TODO
-# testen des servers mit postman
-# build auf den selben server legen
-# data from var 
-# data from file
-# prüfen ob git ordner kopieren kann
-# daten speichern in file
-# Fragen
-# welches format
+def format_string(graph):
+    s = str(graph)
+    s = s.replace("'", '"').replace(': ', ':').replace('False', 'false').replace('True', 'true')\
+        .replace(', ', ',').replace(':u"', ':"')
+    return s
+
+### prod server
+def get_graph(userData=[]):
+    graph = compute_graph(userData)
+    return format_string(graph)
 
 """
-def save_obj(obj, name):
-    with open(name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
-def load_obj(name):
-    with open('obj/' + name + '.pkl', 'rb') as f:
-        return pickle.load(f)
-"""
-
+### dev Server
 def get_graph(userData = []):
-    if userData:
-        print(b"not empty")
-        print(userData)
-    else: 
-        print("empty body")
     filename = "data/response_data.txt"
     with open(filename, "rb") as f: 
         return f.read()
+"""
 
 ## MyHTTPHandler beschreibt den Umgang mit HTTP Requests
 class MyHTTPHandler(BaseHTTPRequestHandler):
@@ -56,40 +47,32 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
         Bei "/" wird index.html gesendet
         """
 
-        # log requests
-        #print(self.path)
-        #print(time.asctime(), '%s: %s' % (self.command, self.path), 'more to log???') 
-        
         if(self.path == "/"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            with open('index.html', 'rb') as f:
+            f = '/index.html'
+        else: 
+            f = self.path
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+        try:
+            with open('public' + f, 'rb') as f:
                 self.wfile.write(f.read())
+        except FileNotFoundError as err:
+            print(err)
+            self.send_error(402, 'file notsadasdasdasd found')
+        except IOError as err:
+            print(err)
+            self.send_error(404, 'file not found')
 
-        """
-        if(self.path == "/api/v1/dataFromFile"):
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.send_header('Access-Control-Allow-Origin', '*')
-            #self.send_header('Access-Control-Allow-Origin', self.headers['origin'])
-            self.end_headers()
-            print(self.headers)
-            #self.wfile.write(b"test")  #body zurueckschicken
-
-            
-            self.wfile.write(get_graph())
-        """
     def do_POST(self):
         """
         definiert den Umgang mit POST Requests
         Liest den Body aus - gibt in zum konvertieren weiter
     
         """
-        if(self.path == "/api/v1/dataFromFile"):   
-            #if (self.headers['Content-type'].split(";")[0] != "application/x-turtle"): #prueft Header auf Standart von Gerbil
-                #self.send_error(415, "Fehler im Header: Content-type")
-                #return
+        if(self.path == "/api/v1/dataFromFile"):
 
             ### POST Request Header ### 
             self.send_response(200)
@@ -99,38 +82,21 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
 
             # get body from request
             content_len = int(self.headers['Content-Length'])
-            body = self.rfile.read(content_len) 
+            body = self.rfile.read(content_len)
 
-            # convert body to list 
-            data = json.loads(str(body, encoding='utf-8'))
-            #print(data)
-
-            """
-            ### Datensatz speichern ###
-            if(save_to_file):
-                filename = "data/response_data.txt"
-                os.makedirs(os.path.dirname(filename), exist_ok=True)   #erstellt file und Ordner falls nicht vorhanden
-                with open(filename, "w") as f: 
-                    f.write(body.decode('utf-8'))
-                    f.close()
-                    print("File ", filename, " saved")
-            """
+            # convert body to list
+            data = json.loads(str(body).decode('utf-8'))  # python 2
+            # data = json.loads(str(body, encoding='utf-8'))      # python 3
+            # print(data)
 
             ## get new graph
             data = get_graph(data)
-
-            
-            #print(type(data))
-
-            #data = json.dumps(data)
-            #print(type(data))
 
             self.wfile.write(data)  #body zurueckschicken
 
         return
  
 if __name__ == "__main__":
- 
     # config
     HOST_NAME = ""
     PORT_NUMBER = 8000
